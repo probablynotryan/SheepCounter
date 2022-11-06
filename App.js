e447dbf2918fad5843c900db8082e36b60904edb
Ryan Bass <theryanbass@gmail.com>
2022-11-06 15:17:58 -0800

    add timer to change component from startup to mainwindow

--- App.js
+++ App.js
@@ -1,47 +1,32 @@
-import React from 'react';
-import { Text, View, Image, StyleSheet} from 'react-native';
+import React, { useEffect } from 'react';
+import { Text, View, Image, StyleSheet, TabBarIOS} from 'react-native';
+import StartupScreen from './StartupScreen';
+import AppScreen from './AppScreen';
 
-import ScrollingBackground from 'react-native-scrolling-images';
+let CurrentComponent = StartupScreen;
 
 export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      currentWindow: 'Welcome Screen'
+    }
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      CurrentComponent = AppScreen;
+      this.setState(
+        {currentWindow: 'Main Window'}
+      )
+    }, 3500)
+  }
+
   render() {
     return (
-   
-        <View style={styles.container}>
-          <ScrollingBackground
-            style={styles.scrollingBackground}
-            speed={60}
-            direction={"up"}
-            images={[require("./assets/sheep.png")]}
-            useNativeDriver={true}
-          />
-          <View style={styles.welcomecircle}>
-        <Image style={styles.actualcircle} source={require('./assets/welcomecircle.png')}
-        resizeMode='contain' />
-        </View>
-        </View>
+      <View style={{flex: 1}}>
+        <CurrentComponent />
+      </View>
   
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    justifyContent: "center",
-    width: "100%",
-    height: "100%"
-  },
-  scrollingBackground: {
-    backgroundColor: "#0B7483"
-  },
-  welcomecircle: {
-    flex: 1,
-    position: "absolute",
-    alignItems: "center", 
-    justifyContent: "center"
-  },
-  actualcircle: {
-    width: 420
-  }
-});
